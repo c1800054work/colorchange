ba7a48980748355546fcc03e7b9ebc0fae9edd4f
peggytsai <pegy2781@hotmail.com>
2018-05-22 18:08:42 +0800

    switch function

--- colorchange/ViewController.swift
+++ colorchange/ViewController.swift
@@ -10,16 +10,72 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var jakeBackgroundView: UIImageView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
+    
+    @IBAction func colorSliderChange(_ sender: Any) {
+        jakeBackgroundView.backgroundColor = UIColor (red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+    }
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var alphaLabel: UILabel!
+
+    @IBAction func redSwitchAction(_ sender: UISwitch) {
+        if sender.isOn == false {
+            redSlider.isEnabled = false
+            redLabel.isEnabled = false
+        } else {
+            redSlider.isEnabled = true
+            redLabel.isEnabled = true
+        }
+    }
+
+    @IBAction func greenSwitchAction(_ sender: UISwitch) {
+        if sender.isOn == false {
+            greenSlider.isEnabled = false
+            greenLabel.isEnabled = false
+        } else {
+            greenSlider.isEnabled = true
+            greenLabel.isEnabled = true
+        }
+    }
+    
+    @IBAction func blueSwitchAction(_ sender: UISwitch) {
+        if sender.isOn == false {
+            blueSlider.isEnabled = false
+            blueLabel.isEnabled = false
+        } else {
+            blueSlider.isEnabled = true
+            blueLabel.isEnabled = true
+        }
+    }
+    
+    @IBAction func alphaSwitchAction(_ sender: UISwitch) {
+        if sender.isOn == false {
+            alphaSlider.isEnabled = false
+            alphaLabel.isEnabled = false
+        } else {
+            alphaSlider.isEnabled = true
+            alphaLabel.isEnabled = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBOutlet weak var colorLabel: UILabel!
+    
 }
 
